f06765c37de1aa5025e0833c330fe16649a7c010
SauravChittal <studentsaurav@gmail.com>
2022-04-15 22:29:43 -0500

    Added the commit messages to hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+import pygit2
+from pygit2 import Repository
+
+repo = Repository('.git')
+last = repo[repo.head.target]
+
 print("""<html>
 <body>
 <p>Hello, Roland!<p/>""")
 
 print("""
 <p>Git log is:
-<pre>
-TODO: add git log here
+<pre>""")
+
+for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
+	print(commit.message)
+
+print("""
 </pre>
 </p>
 """)
